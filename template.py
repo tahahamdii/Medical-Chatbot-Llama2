0d48c205a207da970742dc823e1aa19fea61fad0
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-24 15:46:46 +0200

    list of files logic

--- template.py
+++ template.py
@@ -2,4 +2,17 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompot.py",
+    ".env",
+    "setup.py",
+    "research/trials.ipynb",
+    "app.py",
+    "store_index.py",
+    "static",
+    "templates/chat.html"
+]
