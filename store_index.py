2f94c8a9603ca0c82ac1568d6d41d2d3474927f0
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-25 14:12:54 +0200

    setup file and store index for vectorstor

---
+++ store_index.py
@@ -0,0 +1,14 @@
+from src.helper import load_pfd,text_split,download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+print(PINECONE_API_KEY)
+print(PINECONE_API_ENV)
+
