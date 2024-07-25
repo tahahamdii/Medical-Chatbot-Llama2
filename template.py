8755776da3936734eee76412503cca6082c34eb1
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-25 13:23:26 +0200

    file creation

--- template.py
+++ template.py
@@ -24,3 +24,10 @@ for filepath in list_of_files:
     if filedir !="":
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"creating directory; {filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filename) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
+    else:
+    logging.info(f"{filename} is already created")
