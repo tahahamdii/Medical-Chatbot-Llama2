d0cb4370e31dd3378e4e1f26d9c84d90494f1c23
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-25 13:44:13 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Medical Project',
+    version='0.0.0',
+    author='Mohamed Taha Hamdi',
+    author_email='hamdi.taha@esprit.tn',
+    packages= find_packages(),
+    install_requires= []
+)
