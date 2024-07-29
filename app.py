d0aafa830ef3055588bcd8956984142c7cb89295
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-29 16:10:48 +0200

    pinecont init outdated

--- app.py
+++ app.py
@@ -10,3 +10,13 @@ from src.prompot import *
 import os
 
 app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+pinecone.init(api_key = PINECONE_API_KEY,
+              environment = PINECONE_API_ENV)
+
+index_name="mdical bot"
