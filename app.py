283c909c8d9f6071d3334bc7b0682be7abdb8937
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-29 16:03:12 +0200

    Startin the flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,jsonify,request
+from src.helper import download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+from src.prompot import *
+import os
+
+app = Flask(__name__)
