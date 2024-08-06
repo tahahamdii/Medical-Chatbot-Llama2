cab94ac76a8cfbf1a471116659bfc452e8f066a9
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-06 21:29:07 +0200

    store index data loading chunks and embedding

--- store_index.py
+++ store_index.py
@@ -7,8 +7,10 @@ import os
 load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
 
 print(PINECONE_API_KEY)
-print(PINECONE_API_ENV)
+
+extracted_data = load_pfd("data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
 
