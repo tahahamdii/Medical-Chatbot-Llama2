9a7067521e4767c6c560f27e9a22625cb35120be
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-06 21:31:08 +0200

    initializing the Pinecone

--- store_index.py
+++ store_index.py
@@ -3,6 +3,8 @@ from langchain.vectorstores import Pinecone
 import pinecone
 from dotenv import load_dotenv
 import os
+from pinecone import Pinecone
+
 
 load_dotenv()
 
@@ -13,4 +15,15 @@ print(PINECONE_API_KEY)
 extracted_data = load_pfd("data/")
 text_chunks = text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
+
+
+#Initializing the Pinecone
+pc = Pinecone(api_key='c7eca03a-c885-49a2-bbd9-b4bc52fa3157')
+pc.list_indexes()
+index_name="medicalchatbot"
+index = pc.Index(index_name)
+
+g
+
+
 
