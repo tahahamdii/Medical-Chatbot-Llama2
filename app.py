6374638aa7f2fe7de101bcf17b550ef290ca7a30
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-06 22:00:24 +0200

    prompt model loading and retrieval from vectore store

--- app.py
+++ app.py
@@ -27,3 +27,23 @@ index = pc.Index(index_name)
 
 #Loading the vecotr with the new update langchain pinecone a retour
 vector_store = PineconeVectorStore(index=index,embedding=embeddings)
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs={"prompt": PROMPT}
+
+llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config={'max_new_tokens':512,
+                          'temperature':0.8
+                          })
+
+qa=RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vector_store.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
