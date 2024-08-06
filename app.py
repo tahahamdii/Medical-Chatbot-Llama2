7ddaf4315998a303d9010dc87bf74dc62827e388
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-06 21:56:19 +0200

    Loading the vecotr with the new update langchain pinecone

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
+from langchain_pinecone import PineconeVectorStore
 from src.prompot import *
 from pinecone import Pinecone
 
@@ -17,9 +18,12 @@ load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 
+embeddings = download_hugging_face_embeddings()
 
 pc = Pinecone(api_key='c7eca03a-c885-49a2-bbd9-b4bc52fa3157')
 pc.list_indexes()
 index_name="medicalchatbot"
 index = pc.Index(index_name)
 
+#Loading the vecotr with the new update langchain pinecone a retour
+vector_store = PineconeVectorStore(index=index,embedding=embeddings)
