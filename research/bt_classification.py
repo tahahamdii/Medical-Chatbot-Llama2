b745207b4b3b83ee08d972cc630cd883e899b8b3
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-07 16:15:19 +0200

    dataset import

--- research/bt_classification.py
+++ research/bt_classification.py
@@ -34,3 +34,74 @@ from tensorflow.keras.models import Model, load_model, save_model
 
 from google.colab import drive
 drive.mount('/content/drive')
+
+data = pd.read_csv('/content/drive/MyDrive/lgg-mri-segmentation/Dataset/data.csv')
+data.info()
+
+age_counts = data["age_at_initial_pathologic"].value_counts()
+fig = px.bar(age_counts, title="Age of patients")
+fig.update_layout(
+    xaxis_title="Age",
+    yaxis_title="Frequency",
+    title_x=0.5,
+    showlegend=False
+)
+fig.show()
+
+# This shows the first 10 rows of the patient data
+data.head(10)
+
+data_map = []
+for sub_dir_path in glob.glob("/content/drive/MyDrive/lgg-mri-segmentation/Dataset/" + "*"):
+    # if os.path.isdir(sub_path_dir):
+    try:
+        dir_name = sub_dir_path.split('/')[-1]
+        for filename in os.listdir(sub_dir_path):
+            image_path = sub_dir_path + '/' + filename
+            data_map.extend([dir_name, image_path])
+    except Exception as e:
+        print(e)
+
+df = pd.DataFrame({"patient_id": data_map[::2],
+                   "path": data_map[1::2]})
+df.head()
+
+# Path to the images and the mask images of the Brain MRI
+df_imgs = df[~df['path'].str.contains("mask")]
+df_masks = df[df['path'].str.contains("mask")]
+
+# File path line length images for later sorting
+BASE_LEN = 89  #
+END_IMG_LEN = 4  #
+END_MASK_LEN = 9  #
+
+# Data sorting
+imgs = sorted(df_imgs["path"].values, key=lambda x: int(x[BASE_LEN:-END_IMG_LEN]))
+masks = sorted(df_masks["path"].values, key=lambda x: int(x[BASE_LEN:-END_MASK_LEN]))
+
+# Sorting check
+idx = random.randint(0, len(imgs) - 1)
+print("Path to the Image:", imgs[idx], "\nPath to the Mask:", masks[idx])
+
+### 1.3 Creating the final data frame:
+
+# Make a dataframe with the images and their corresponding masks and patient ids
+# Final dataframe
+brain_df = pd.DataFrame({"patient_id": df_imgs.patient_id.values,
+                         "image_path": imgs,
+                         "mask_path": masks
+                         })
+
+
+# Make a function that search for the largest pixel value in the masks, because that will indicate if the image have
+# a corresponding mask with a tumor or not , also add this column to the dataframe
+def pos_neg_diagnosis(mask_path):
+    value = np.max(cv2.imread(mask_path))
+    if value > 0:
+        return 1
+    else:
+        return 0
+
+
+brain_df['mask'] = brain_df['mask_path'].apply(lambda x: pos_neg_diagnosis(x))
+brain_df
