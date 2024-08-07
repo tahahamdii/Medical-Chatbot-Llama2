ddd93ba68910de27976a43cf0caa3478c530c2a1
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-07 16:36:52 +0200

    BUILDING SEGMENTATION MODEL TO LOCALIZE TUMOR UNET MODEL

--- research/bt_classification.py
+++ research/bt_classification.py
@@ -440,3 +440,51 @@ print(report)
 print("Confusion Matrix of CNN model")
 plt.figure(figsize = (5,5))
 sns.heatmap(cm, annot=True);
+
+
+def train_generator(data_frame, batch_size, aug_dict,
+                    image_color_mode="rgb",
+                    mask_color_mode="grayscale",
+                    image_save_prefix="image",
+                    mask_save_prefix="mask",
+                    save_to_dir=None,
+                    target_size=(256, 256),
+                    seed=1):
+    image_dg = ImageDataGenerator(**aug_dict)
+    mask_dg = ImageDataGenerator(**aug_dict)
+
+    image_generator = image_dg.flow_from_dataframe(
+        data_frame,
+        color_mode=image_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=image_save_prefix,
+        x_col="image_path",
+        class_mode=None,
+        seed=seed)
+
+    mask_generator = mask_dg.flow_from_dataframe(
+        data_frame,
+        color_mode=mask_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=mask_save_prefix,
+        x_col="mask_path",
+        class_mode=None,
+        seed=seed)
+
+    train_gn = zip(image_generator, mask_generator)
+
+    for (pic, mask) in train_gn:
+        pic, mask = datachanges(pic, mask)
+        yield (pic, mask)
+
+
+def datachanges(pic, mask):
+    mask = mask / 255
+    mask[mask > 0.5] = 1
+    mask[mask <= 0.5] = 0
+    pic = pic / 255
+    return (pic, mask)
